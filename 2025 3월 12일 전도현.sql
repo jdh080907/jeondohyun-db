022b273a4997b7e706438e9d8990919618ce04f5
jdh080907 <193445773+jdh080907@users.noreply.github.com>
2025-03-12 12:48:36 +0900

    Add files via upload

---
+++ 2025 3월 12일 전도현.sql
@@ -0,0 +1,13 @@
+create table address(
+ address 1 varchar(255),
+ address 2 varchar(255),
+ address 3 varchar(255),
+ postai_code varchar(255),
+ client_id varchar(255));
+ 
+ 
+ insert into address  values("안양시",'동안구',학의로''12345','이름);//테이블 삽입
+ 
+ 
+ drop table address;  //테이블 삭제
+ 
